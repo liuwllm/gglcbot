38b3c9556d0005766722f068c4be9d311c2f070a
liuwllm <liu.wllm@gmail.com>
2024-09-25 19:36:27 -0400

    Add Python scraper for Leetcode Questions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { CronJob } from 'cron';
 
 const token = process.env.DISCORD_TOKEN;
 
@@ -13,5 +14,26 @@ client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
+
+const gcpClient = authorize().then(listEvents);
+
+function scheduleMessage(lcQuestion) {
+    let job = new CronJob(
+        '48 16 * * *', // cron expression that describes when the function below is executed
+        function() {
+            channel.send(msgToSend); //insert here what you want to do at the given time
+        },
+        null,
+        true,
+        'America/Los_Angeles' //insert your server time zone here
+    );
+}
+
+client.once(Events.ClientReady, c => {
+   console.log('Ready! Logged in as ', c.user.tag);
+   scheduleMessage(element.title, element.readabledate);
+});
+
 // Log in to Discord with your client's token
 client.login(token);
+
---
+++ utils/scraper.py
@@ -0,0 +1,48 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+# options.add_argument("--headless")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get('https://neetcode.io/practice')
+
+elements = driver.find_elements(By.CLASS_NAME, 'tab-link')
+
+allButton = elements[3]
+allButton.click()
+
+# viewButton = driver.find_elements(By.CLASS_NAME, 'button navbar-btn is-rounded is-info is-outlined has-tooltip-bottom')
+
+links = driver.find_elements(By.XPATH, "//a[starts-with(@href, 'https://leetcode.com')]")
+buttons = driver.find_elements(By.ID, "diff-btn")
+
+output = []
+
+for i in range(len(links)):
+    link_text = links[i].get_attribute('href')
+
+    name = link_text[len('https://leetcode.com/problems/'):].rstrip("/").split("-")
+    name = " ".join(name)
+    name = name.title()
+
+    difficulty = buttons[i].text
+
+    question = {
+        "name": name,
+        "link": link_text,
+        "difficulty": difficulty,
+    }
+
+    output.append(question)
+
+with open('lcdb.json', 'w') as json_file:
+    json.dump(output, json_file, indent=4)
+
+print(f'Retrieved problems: {str(len(output))}')
